2ccd8618a67b5cce330c6cd08959770c753e1939
JV <jvcladera@gmail.com>
2023-02-06 09:35:34 +0100

    1_1_create_DDBB, must actualize 1_create_DDBB

---
+++ 1_1_create_DDBB.R
@@ -0,0 +1,48 @@
+directory <- getwd()
+#setwd(paste0(directory, "/Data/"))
+
+library("tidyverse")
+all_metadata_links <- tibble(read.csv(paste0(directory, "/all_metadata_links.csv"), fileEncoding = "UTF-8"))
+
+col_names <- colnames(all_metadata_links)
+
+### son aquelles que no aporten cap info 
+delete <- c("X", "X.1", "AGGREGALLOWED", "AUTOPEN", "AXIS.VERSION", "CHARSET", "COPYRIGHT",
+              "DESCRIPTIONDEFAULT", "CONTVARIABLE", "CONTVARIABLE.ca.", "LANGUAGE", "LANGUAGES")
+### son aquelles que no aporten cap info
+
+
+### no se que hi ha...
+maybe_delete <- c("CODES", "CODES.ca.", "MAP", "MAP.ca.", "PRECISION", "PRECISION.ca.", "CONTACT", "CONTACT.ca.")
+###
+
+col_names <- setdiff(col_names, delete)
+col_names <- setdiff(col_names, maybe_delete)
+
+
+# for(i in 1:length(col_names)){
+#   print(paste0("Nom columna :", col_names[i]))
+#   unique_data <- unique(dades[col_names[i]])
+#   num_data <- nrow(unique_data)
+#   print(paste0("Hi ha ", num_data, " dades uniques a la columna"))
+#   #print(head(unique(dades[col_names[i]]), 10))
+# }
+
+imp_metadata_links <- all_metadata_links[col_names]
+
+write.csv(imp_metadata_links,"imp_metadata_links.csv", fileEncoding = "UTF-8")
+
+
+######
+
+
+links_to_actualize <- imp_metadata_links[imp_metadata_links$"UPDATE.FREQUENCY" != "No se actualiza" &
+                                           !is.na(imp_metadata_links$"UPDATE.FREQUENCY"), ]
+
+links_to_actualize <- links_to_actualize[c("Titol_cat", "Enllac", "Noms_dades")]
+names(links_to_actualize) <- c("Titol_cat", "Enllac", "Nom_BBDD")
+
+
+write.csv(links_to_actualize, "links_to_actualize.csv", fileEncoding = "UTF-8")
+
+       
